c8a6b43dc8404e13546e2df1bcc5c9b448d60f9e
森田雄太郎 <yu8ta8ro@icloud.com>
2017-01-31 17:55:36 +0900

    modified add-on

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['app62982076@heroku.com'],
-  :password       => ENV['osf1arpe3293'],
-  :domain         => 'intense-eyrie-53145.herokuapp.com',
-  :enable_starttls_auto => true
-}
