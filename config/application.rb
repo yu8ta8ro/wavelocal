cd804878a0eb1b83d08351eade54e6c409ec0ca1
森田雄太郎 <yu8ta8ro@icloud.com>
2017-03-27 18:30:32 +0900

    changed assets on heroku

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.serve_static_assets = true
 
 module Wavelocal
   class Application < Rails::Application
@@ -25,6 +24,7 @@ module Wavelocal
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+    config.serve_static_assets = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
