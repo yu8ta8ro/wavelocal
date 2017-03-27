5fb143602cecf7494e135e0623ac691cabb47652
森田雄太郎 <yu8ta8ro@icloud.com>
2017-03-27 13:49:29 +0900

    setting in environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -76,19 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # config.action_mailer.default_url_options = { :host => 'us-cdbr-iron-east-02.cleardb.net' }
-
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['app62982076@heroku.com'],
-  #   :password       => ENV['osf1arpe3293'],
-  #   :domain         => 'intense-eyrie-53145.herokuapp.com',
-  #   :enable_starttls_auto => true
-  # }
 
 end
 
