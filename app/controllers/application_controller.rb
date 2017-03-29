0dfc5fc1378a04c3c332c52c3674a8711575340d
森田雄太郎 <yu8ta8ro@icloud.com>
2017-03-29 11:17:16 +0900

    Set after_sign_in_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :lastname, :firstname, :student, :others, :address])
   end
+
+  def after_sign_in_path_for(resource)
+    companyeets_path
+  end
 end
